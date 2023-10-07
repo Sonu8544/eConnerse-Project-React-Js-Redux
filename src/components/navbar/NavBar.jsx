ff3f9fdbaf45ac403719ad77e3966f7d8146c52c
Sonu Kumar <sksonu8544@gmail.com>
2023-10-08 01:20:29 +0530

    Link Added in NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -7,12 +8,16 @@ const NavBar = () => {
       <div className="wrapper w-full bg-slate-500 p-3 text-white text-2xl ">
         <header className="container flex items-center w-full justify-around ">
           <div className="name">
-            <h1 className="text-orange-500 text-3xl">eCommerse</h1>
+            <Link to="/">
+              <h1 className="text-orange-500 hover:underline text-3xl">
+                eCommerse
+              </h1>
+            </Link>
           </div>
           <div>
             <div className="flex items-center justify-center">
               <input
-                className="px-4 text-orange-500 py-2 border-4 border-orange-500 rounded outline-none w-[400px] text-lg rounded-xl"
+                className="px-4 text-orange-500 py-2 border-4 border-orange-500 rounded outline-none w-[400px] text-lg"
                 type="text"
                 placeholder="search here..."
               />
@@ -23,8 +28,12 @@ const NavBar = () => {
           </div>
           <div>
             <div className="addToCart">
-               <p className="ml-12 text-white " > 13 </p>
-              <h1 className="text-orange-500">Add To Cart</h1>
+              <p className="ml-12 text-white "> 13 </p>
+              <Link to="/cart">
+                <h1 className="text-orange-500 hover:underline duration-100">
+                  Add To Cart
+                </h1>
+              </Link>
             </div>
           </div>
         </header>

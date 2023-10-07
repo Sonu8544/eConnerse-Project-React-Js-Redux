36de8b7e82a11ffa6537daf06fbe6ce21fc57680
Sonu Kumar <sksonu8544@gmail.com>
2023-10-07 15:02:58 +0530

    Routing Complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Products } from "./components/products";
+import { Cart } from "./components/cart";
+import { Product } from "./components/product";
+import { NotFound } from "./components/not-found";
 
 function App() {
-
   return (
     <>
-     <h1 className='text-orange-600 text-3xl' >Hello eCommerse...</h1>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/home" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

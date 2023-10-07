6906bc4e395efc4798dc8739239bd1bcf2b0a9bd
Sonu Kumar <sksonu8544@gmail.com>
2023-10-08 01:50:44 +0530

    NavBar ready With Funclaty

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ onSearch, cartItemCount = 3 }) => {
+ 
+  const [searchQuery, setSearchQuery] = useState("")
+
+  const handleSubmit = () =>{
+    if(searchQuery.trim().length){
+      onSearch(searchQuery.trim())
+    }
+    setSearchQuery("")
+  }
+
   return (
     <>
       <div className="wrapper w-full bg-slate-500 p-3 text-white text-2xl ">
@@ -19,15 +29,20 @@ const NavBar = () => {
               <input
                 className="px-4 text-orange-500 py-2 border-4 border-orange-500 rounded outline-none w-full md:w-[400px] text-lg"
                 type="text"
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
                 placeholder="Search here..."
               />
-              <button className="bg-orange-400 hover:bg-orange-600 text-white rounded px-4 py-2 ml-2 transition duration-300">
+              <button type="button" className="bg-orange-400 hover:bg-orange-600 text-white rounded px-4 py-2 ml-2 transition duration-300" onClick={handleSubmit}>
                 Search
               </button>
             </div>
           </div>
           <div className="addToCart">
-            <p className="ml-0 md:ml-12 text-white">13</p>
+          {cartItemCount > 0 && (
+            <p className="ml-0 md:ml-12 text-white">{cartItemCount < 9 ? cartItemCount : "9+"}</p>
+          )}
+            
             <Link to="/cart">
               <h1 className="text-orange-500 hover:underline duration-100">
                 Add To Cart
